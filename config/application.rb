12c17f9fa836ebb3f1cef685a975dd9d3020f4c6
sgaMunicipalAffairs <60800241+sgaMunicipalAffairs@users.noreply.github.com>
2020-03-08 18:08:25 -0500

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module CSCE431Apartmentrater
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.fallbacks = true
   end
 end
